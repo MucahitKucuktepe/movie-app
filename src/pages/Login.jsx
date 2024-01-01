176536365ea6eeea596176c4d04e0f0f1f21e620
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-01-01 11:15:51 +0300

    login added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState } from "react";
+import GoogleIcon from "../assets/icons/GoogleIcon";
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login </div>
-  )
-}
+ 
 
-export default Login
+  return (
+    <div className="flex justify-center">
+      <div className="overflow-hidden flex-1  justify-center items-center">
+        <div className={`form-container mt-[5vh] w-[380px] h-[500px] `}>
+        <form>
+            <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3">
+              Sign In
+            </h2>
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                name="email"
+                className="peer"
+                type="email"
+                placeholder=" "
+                required
+            />
+              <label htmlFor="floating_email">Email</label>
+            </div>
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                name="password"
+                className="password"
+                type="password"
+                placeholder=" "
+                required
+               
+              />
+              <label htmlFor="floating_password">Password</label>
+            </div>
+            <div className="flex justify-between">
+              <span
+                className="py-3 font-[0.75rem] cursor-pointer decoration-none text-gray-500 hover:text-red-500"
+              
+              >
+                Forgot Password
+              </span>
+              <Link
+                to="/register"
+                className="py-3 font-[0.75rem] cursor-pointer decoration-none text-gray-500 hover:text-red-500"
+              >
+                Sign Up
+              </Link>
+            </div>
+            <button className="btn-danger" type="submit">
+              Login
+            </button>
+            <button
+              className="flex justify-between text-center items-center btn-danger"
+              type="button"
+          
+            >
+              Continue with Google
+              <GoogleIcon color="currentColor" />
+            </button>
+            <button
+              className="flex justify-between text-center items-center btn-danger"
+              type="button"
+          
+            >
+              Continue with Github
+              <FaGithub className="text-[2rem]" color="currentColor" />
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
