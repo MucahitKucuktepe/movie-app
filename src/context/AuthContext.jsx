970c60b7511b4d80c2fa188abf302b7f2d8ad066
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-01-01 18:23:26 +0300

    SingIn function added to login page

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,29 +1,48 @@
 import React, { createContext, useContext } from "react";
 import { auth } from "../auth/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext();
 export const useAuthContext = () => {
   return useContext(AuthContext);
 };
 
 const AuthContextProvider = ({ children }) => {
-  const createUser = async (email, password,displayName) => {
+  const navigate=useNavigate()
+  const createUser = async (email, password, displayName) => {
     try {
-      const userCredential =
-        await createUserWithEmailAndPassword(
-          auth,
-          email,
-          password
-        );
-        console.log(userCredential)
-        console.log(displayName)
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(userCredential);
+      console.log(displayName);
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
   };
-  const values={
-    createUser
-  }
+  const signIn = async (email, password) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(userCredential);
+      navigate("/")
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const values = {
+    createUser,
+    signIn,
+  };
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import GoogleIcon from "../assets/icons/GoogleIcon";
 import { FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useAuthContext } from "../context/AuthContext";
 
 const Login = () => {
- 
+  const { signIn } = useAuthContext();
+  const [info, setInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInfo({ ...info, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = info;
+    signIn(email, password);
+  };
 
   return (
     <div className="flex justify-center">
       <div className="overflow-hidden flex-1  justify-center items-center">
         <div className={`form-container mt-[5vh] w-[380px] h-[500px] `}>
-        <form>
+          <form onSubmit={handleSubmit}>
             <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3">
               Sign In
             </h2>
@@ -21,7 +35,8 @@ const Login = () => {
                 type="email"
                 placeholder=" "
                 required
-            />
+                onChange={handleChange}
+              />
               <label htmlFor="floating_email">Email</label>
             </div>
             <div className="relative z-0 w-full mb-6 group">
@@ -30,16 +45,13 @@ const Login = () => {
                 className="password"
                 type="password"
                 placeholder=" "
+                onChange={handleChange}
                 required
-               
               />
               <label htmlFor="floating_password">Password</label>
             </div>
             <div className="flex justify-between">
-              <span
-                className="py-3 font-[0.75rem] cursor-pointer decoration-none text-gray-500 hover:text-red-500"
-              
-              >
+              <span className="py-3 font-[0.75rem] cursor-pointer decoration-none text-gray-500 hover:text-red-500">
                 Forgot Password
               </span>
               <Link
@@ -55,7 +67,6 @@ const Login = () => {
             <button
               className="flex justify-between text-center items-center btn-danger"
               type="button"
-          
             >
               Continue with Google
               <GoogleIcon color="currentColor" />
@@ -63,7 +74,6 @@ const Login = () => {
             <button
               className="flex justify-between text-center items-center btn-danger"
               type="button"
-          
             >
               Continue with Github
               <FaGithub className="text-[2rem]" color="currentColor" />
